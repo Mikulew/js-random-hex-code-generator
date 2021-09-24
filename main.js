1b1889e69c67c4f3858b71243e50910774d4e922
Mikulew <mikulew@gmail.com>
2021-09-24 11:15:12 +0200

    Add UI and logic

---
+++ main.js
@@ -0,0 +1,29 @@
+const colorPreview = document.querySelector(".color-preview");
+const output = document.getElementById("output");
+const generateButton = document.getElementById("generate");
+const copyButton = document.getElementById("copy");
+const toast = document.querySelector(".toast");
+const hexString = "0123456789abcdef";
+
+function generateHexCode() {
+  let hexCode = "#";
+  for (let i = 0; i < 6; i++) {
+    hexCode += hexString[Math.floor(Math.random() * hexString.length)];
+  }
+  output.value = hexCode;
+  colorPreview.classList.remove("color-animation");
+  setTimeout(() => colorPreview.classList.add("color-animation"), 10);
+  colorPreview.style.backgroundColor = hexCode;
+}
+
+function copyHexCode() {
+  output.select();
+  document.execCommand("copy");
+  toast.style.transform = "translateX(0)";
+  setTimeout(() => toast.style.transform = "translateX( calc(100% + 10px) )", 2000);
+}
+
+window.onload = generateHexCode;
+
+generateButton.addEventListener("click", generateHexCode);
+copyButton.addEventListener("click", copyHexCode);
